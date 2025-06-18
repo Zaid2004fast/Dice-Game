023cc8e817178d42b5ef6e0e0c2505b677eb1d8f
Zaid Bin Umer <168066774+Zaid2004fast@users.noreply.github.com>
2025-06-18 23:07:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var n = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "./images/dice" + n + ".png");
+
+var m = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "./images/dice" + m + ".png");
+
+
+if(n>m)
+{
+    document.querySelector("h1").textContent="🚩Player1 Wins";
+}
+else if(m>n)
+{
+document.querySelector("h1").textContent="Player2 Wins🚩";
+}
+else{
+    document.querySelector("h1").textContent="Draw";
+}
